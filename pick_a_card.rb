9096b43c90aaa0454d3aa5358d44131ec27c0038
SaRuMan <ruslan.soltaev@gmail.com>
2020-03-28 22:36:40 +0300

    Add russian prhase in outpit

--- pick_a_card.rb
+++ pick_a_card.rb
@@ -3,4 +3,4 @@ values = %w[2 3 4 5 6 7 8 9 10 Волет Королева Король Туз]
 #Масти для карт
 suits  = %w[Буби Черви Крести Пики]
 some_card = values.sample + " " + suits.sample
-puts "Some card: #{some_card}"
+puts "Случайная карта: #{some_card}"
