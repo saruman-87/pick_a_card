3ddc1fa62b165fcd4900041c653931532c397eac
SaRuMan <ruslan.soltaev@gmail.com>
2020-03-28 19:36:34 +0300

    Russian version 1.0

--- pick_a_card.rb
+++ pick_a_card.rb
@@ -1,6 +1,6 @@
 #Значение карты
-values = %w[2 3 4 5 6 7 8 9 10 J Q K A]
+values = %w[2 3 4 5 6 7 8 9 10 Волет Королева Король Туз]
 #Масти для карт
-suits  = %w[Diamonds Hearts CLubs Spades]
+suits  = %w[Буби Черви Крести Пики]
 some_card = values.sample + " " + suits.sample
 puts "Some card: #{some_card}"
