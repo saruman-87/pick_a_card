816a0a42f04b49704975d53f136bd85139db7cf6
SaRuMan <ruslan.soltaev@gmail.com>
2020-03-28 22:42:34 +0300

    Version with symbols in Eng

--- pick_a_card.rb
+++ pick_a_card.rb
@@ -1,6 +1,6 @@
 #Значение карты
-values = %w[2 3 4 5 6 7 8 9 10 Волет Королева Король Туз]
+values = %w[2 3 4 5 6 7 8 9 10 J Q K A]
 #Масти для карт
-suits  = %w[Буби Черви Крести Пики]
-some_card = values.sample + " " + suits.sample
-puts "Случайная карта: #{some_card}"
+suits  = %w[♦ ♠ ♥ ♣]
+some_card = values.sample + suits.sample
+puts "Card #{some_card}"
