a4ecfec1926de427ac34b1bb4fcb263e3dd1fd5f
RuslanSoltaev <ruslan.soltaev@gmail.com>
2023-12-02 12:02:22 +0300

    Обновил print

--- pick_a_card.py
+++ pick_a_card.py
@@ -3,4 +3,4 @@ values = ['2', '3', '4', '5', '6', '7', '8', '9','10','J','Q','K','A']
 suits = ['Diamonds', 'Hearts', 'Clubs', 'Spades'] #Буби, Черви, Крести, Пики
 
 result = f"{random.choice(suits)} {random.choice(values)}"
-print (f'{result}')
+print (result)
