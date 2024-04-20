855eb553a021095915b259ce0f2b34de6ce4e8c1
RuslanSoltaev <ruslan.soltaev@gmail.com>
2024-04-20 08:57:33 +0300

    Russian names cards
    Переписал код на русские название игральных карт

--- main.rb
+++ main.rb
@@ -1,5 +1,5 @@
-values = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
-suits = ['Diamonds', 'Hearts', 'Clubs', 'Spades']
+values = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'Валет', 'Королева', 'Король', 'Туз']
+suits = ['Черви', 'Пики', 'Буби', 'Крести']
 
 random_card = values.sample + ' ' + suits.sample
 puts random_card
