756550aa63d3f0cf53278f4cfeb44dea0d81651f
saruman_87 <ruslan.soltaev@gmail.com>
2024-04-20 08:28:39 +0300

    Initial version

---
+++ main.rb
@@ -0,0 +1,5 @@
+values = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
+suits = ['Diamonds', 'Hearts', 'Clubs', 'Spades']
+
+random_card = values.sample + ' ' + suits.sample
+puts random_card
