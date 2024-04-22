64b2620a37c18712e95c2c295be73669b8d10ee0
saruman_87 <ruslan.soltaev@gmail.com>
2024-04-22 07:15:27 +0300

    Version with Symbols

--- main.rb
+++ main.rb
@@ -1,14 +1,32 @@
 values = ['6', '7', '8', '9', '10', 'Валет', 'Королева', 'Король', 'Туз']
 suits = ['Черви', 'Пики', 'Буби', 'Крести']
+value = values.sample
+suit = suits.sample
 
-random_card = values.sample + ' ' + suits.sample
+def convert_suit_to_symbol(suit)
+    case suit
+    when "Черви"
+      return "♥"
+    when "Пики"
+      return "♠"
+    when "Буби"
+        return "♦"
+    when "Крести"
+        return "♣"
+    end
+end
+
+
+random_card = value + ' ' + suit
 puts "Правильный ввод: 6, 7, 8, 9, 10, Валет, Королева, Король, Туз, Черви, Пики, Буби, Крести"
 puts "Отгодайте карту которую мы загадали: "
+puts "#{random_card}"
 
 user_card = gets.chomp
 if user_card == random_card
     puts "Вы Телепат!"
 else
     puts "Вы не угадали!"
-    puts "Мы загодали #{random_card}"
 end
+
+puts "Мы загодали \"#{value} #{convert_suit_to_symbol(suit)}\""
